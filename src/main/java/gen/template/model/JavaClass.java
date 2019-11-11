9da71e75209f84457b563e4314345e835812f0b0
shanadmin <937503784@qq.com>
2019-11-11 18:23:34 +0800

    功能实现

--- src/main/java/gen/template/model/JavaClass.java
+++ src/main/java/gen/template/model/JavaClass.java
@@ -7,9 +7,15 @@ import java.util.Set;
 @Data
 public class JavaClass {
     //类名
-    private String  name;
+    private String name;
+    //模板名称
+    private String fltName;
     //依赖包集合(不重复集合)
     private Set<String> dependPackages;
     //注解
-    private Set<String> annotationList;
+    private Set<String> annotations;
+    //方法集合
+    private Set<JavaMethod> javaMethods;
+    //变量集合
+    private Set<JavaProperty> javaProperties;
 }
--- src/main/java/gen/template/model/JavaProperty.java
+++ src/main/java/gen/template/model/JavaProperty.java
@@ -10,12 +10,16 @@ public class JavaProperty {
     private String name;
     //类型
     private String type;
+    //是否静态
+    private boolean staticState;
+    //是否常量
+    private boolean finalState;
     //默认值
     private String value;
     //是否映射对应字段
-    private boolean isExist;
+    private boolean exist;
     //是否有构造方法，包含属性给默认值，gmt_update赋值
-    private boolean hasConstrut;
+    private boolean construct;
     //拥有的注解
-    private Set<String> notes;
+    private Set<String> annotations;
 }
